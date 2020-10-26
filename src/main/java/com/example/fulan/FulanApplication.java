ef9c2130a4b2866ecb255ddec0aa935221dd54cc
hemin123456 <18815795189@163.com>
2020-10-26 14:55:20 +0800

    first commit

---
+++ src/main/java/com/example/fulan/FulanApplication.java
@@ -0,0 +1,13 @@
+package com.example.fulan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FulanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fulan/FulanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fulan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
